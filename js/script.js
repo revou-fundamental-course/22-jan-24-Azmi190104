d5b2079775d0c12f4ef1dab881f85740b8cdd31e
unknown <m.azmialfadillah@gmail.com>
2024-01-30 21:24:31 +0700

    responsive web to mobile

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('.nav ul')
+const toggle = document.querySelector('.menu input')
+
+toggle.addEventListener('click', function(){
+    ul.classList.toggle('slide')
+
+})
